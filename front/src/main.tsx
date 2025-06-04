549a7d38c0acfbe84be6326e3a9264a7d9b40800
Lucas de Oliveira Mota <lucas.d.mota@ufv.br>
2025-06-04 11:28:56 -0300

    Cria o provider global

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const queryClient = new QueryClient()
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    {/* 2. Injeta no tree inteiro */}
+    <QueryClientProvider client={queryClient}>
+      <App />
+      {/* Devtools abre com ⌘/Ctrl + K */}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>,
-);
+)
