f046a3e2f7c71b479e46b86abf84a545f895bcdb
Hicaro <hicaro@doo.is>
2025-06-05 20:11:32 -0300

    adicionar integracao do cadastro de usuario

--- front/src/hooks/useUsuarios.ts
+++ front/src/hooks/useUsuarios.ts
@@ -10,6 +10,7 @@ export interface UsuarioDTO {
   id:            number
   nomeCompleto:  string
   email:         string
+  login:         string
   perfil:        PerfilResumo
   ativo:         boolean
   dataCadastro:  string
@@ -18,6 +19,7 @@ export interface UsuarioDTO {
 export interface UsuarioCreate {
   nomeCompleto: string
   email:        string
+  login:        string
   senhaPlain:   string
   perfilId:     number
 }
