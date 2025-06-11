361c68c97143c49ff3b6748d83a4474572cb805e
Lucas de Oliveira Mota <lucas.d.mota@ufv.br>
2025-06-11 09:49:25 -0300

    Adiciona novamente perfis de usuário comentados no V1

---
+++ src/main/resources/db/migration/V16__Inserir_Perfil_Usuario.sql
@@ -0,0 +1,4 @@
+-- Inserções iniciais opcionais para PerfilUsuario
+INSERT INTO PerfilUsuario (nome, descricao) VALUES ('Administrador', 'Acesso total ao sistema');
+INSERT INTO PerfilUsuario (nome, descricao) VALUES ('Supervisor', 'Pode cadastrar produtos, atualizar estoque, receber alertas e emitir relatórios');
+INSERT INTO PerfilUsuario (nome, descricao) VALUES ('Funcionário', 'Pode apenas registrar retiradas de estoque e consultar produtos');
